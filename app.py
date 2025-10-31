78251f22b33af516ea0a11a06b285b20ce61eb34
DivCodez <divyambanga@gmail.com>
2025-10-31 13:52:19 -0400

    Added basic login (No database or verification)

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
 app = Flask(__name__)
+app.secret_key = 'AskMyData'
 #home page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # Need to add verification
+        print(username)
+        session["username"] = username
+        return render_template("logged_in_home.html", username=username)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
 
 print("App is running...")
 if __name__ == "__main__":
