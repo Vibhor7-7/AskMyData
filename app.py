4d5dc4149b05c4366930ccc0aece0ad0c8c6e87e
DivCodez <divyambanga@gmail.com>
2025-10-30 20:20:25 -0400

    fixed skeleton

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+#home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+print("App is running...")
 if __name__ == "__main__":
     app.run()
